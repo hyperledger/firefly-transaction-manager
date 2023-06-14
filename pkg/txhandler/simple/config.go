1f942ec8641854faa0f8e3f4e59220c58289c4a3
Peter Broadhurst <peter.broadhurst@kaleido.io>
2023-06-14 02:05:45 -0400

    Fix lint
    Signed-off-by: Peter Broadhurst <peter.broadhurst@kaleido.io>

--- pkg/txhandler/simple/config.go
+++ pkg/txhandler/simple/config.go
@@ -46,12 +46,11 @@ const (
 	GasOracleModeRESTAPI   = "restapi"
 	GasOracleModeConnector = "connector"
 
-	defaultMaxInFlight       = 100
-	defaultNonceStateTimeout = "1h"
-	defaultInterval          = "10s"
-	defaultRetryInitDelay    = "250ms"
-	defaultRetryMaxDelay     = "30s"
-	defaultRetryFactor       = 2.0
+	defaultMaxInFlight    = 100
+	defaultInterval       = "10s"
+	defaultRetryInitDelay = "250ms"
+	defaultRetryMaxDelay  = "30s"
+	defaultRetryFactor    = 2.0
 )
 
 const (
