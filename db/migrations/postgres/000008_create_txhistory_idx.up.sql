19bd14535173e19dfb4437faf1bf6b9517ebadbb
Peter Broadhurst <peter.broadhurst@kaleido.io>
2023-06-25 17:52:19 -0400

    Drop before create on migrations
    Signed-off-by: Peter Broadhurst <peter.broadhurst@kaleido.io>

--- db/migrations/postgres/000008_create_txhistory_idx.up.sql
+++ db/migrations/postgres/000008_create_txhistory_idx.up.sql
@@ -1,4 +1,6 @@
 BEGIN;
+DROP INDEX IF EXISTS txhistory_id;
+DROP INDEX IF EXISTS txhistory_txid;
 CREATE UNIQUE INDEX txhistory_id ON txhistory(id);
 CREATE INDEX txhistory_txid ON txhistory(tx_id);
 COMMIT;
